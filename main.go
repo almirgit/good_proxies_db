4ba15c7c478ebc64dba160f6a3abf367b22378f6
Developer1 <developer1@4lmir.de>
2024-08-16 00:07:04 +0200

    Use logger handler as shared object

--- main.go
+++ main.go
@@ -7,25 +7,33 @@ import (
 	"good_proxies_go_ai/shared"
 )
 
+func display_error(errmsg string, err error) {
+	shared.Log.Error(errmsg, "error", err)	
+}
+
 func main() {
 
 	//LOG_FILE := os.Getenv("LOG_FILE")
-	logger, logfile := shared.Loginit()
-	defer logfile.Close() // executes on function exit
+	//logger, logfile := shared.Loginit()
+	shared.Log, shared.Logfile = shared.Loginit()
+	defer shared.Logfile.Close() // executes on function exit
 
-	logger.Info("Start")
+	shared.Log.Info("Start")
 
 	// Load configuration
 	cfg, err := config.LoadConfig(".config.yml")
 	if err != nil {
-		logger.Error("Error loading config: %v", err)
+		display_error("Error reading .config.yml", err)
+		//logger.Error("Error reading .config.yml", "error", err)
 		return
 	}
 
 	// Get the database connection
 	db, err := proxy_data_input.DBInConnect(*cfg)
 	if err != nil {
-		logger.Error("Error connecting to database: %v", err)
+		//logger.Error("Error connecting to database: %v", "error", err)
+		display_error("Error connecting to database", err)
+		return
 	}
 	defer db.Close()
 
