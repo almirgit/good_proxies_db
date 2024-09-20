7fdcd32f5f7f56887ab4ea9e3ce1626a6efdac82
Developer1 <developer1@4lmir.de>
2024-09-20 23:47:23 +0200

    Add infinity loop

--- main.go
+++ main.go
@@ -14,38 +14,38 @@ func display_error(errmsg string, err error) {
 func main() {
 
 	//LOG_FILE := os.Getenv("LOG_FILE")
-	//logger, logfile := shared.Loginit()
 	shared.Log, shared.Logfile = shared.Loginit()
 	defer shared.Logfile.Close() // executes on function exit
 
-	shared.Log.Info("Start")
+	for {
+		shared.Log.Info("Start processing cycle")
 
-	// Load configuration
-	cfg, err := config.LoadConfig(".config.yml")
-	if err != nil {
-		display_error("Error reading .config.yml", err)
-		//logger.Error("Error reading .config.yml", "error", err)
-		return
+		// Load configuration
+		cfg, err := config.LoadConfig(".config.yml")
+		if err != nil {
+			display_error("Error reading .config.yml", err)
+			//logger.Error("Error reading .config.yml", "error", err)
+			return
+		}
+
+		// Get the database connection
+		db, err := proxy_data_input.PgDBConnect(*cfg)
+		if err != nil {
+			//logger.Error("Error connecting to database: %v", "error", err)
+			display_error("Error connecting to database", err)
+			return
+		}
+		defer db.Close()
+
+		proxy_list := proxy_data_input.GetProxies(db)
+
+		// // Using a for loop with an index
+		// for i := 0; i < len(proxy_list); i++ {
+		// 	fmt.Println(proxy_list[i])
+		// }
+
+		proxy_data_output.Check_proxies(db, *cfg, proxy_list)
+		proxy_data_output.Check_stored_proxies(db, *cfg)
 	}
 
-	// Get the database connection
-	db, err := proxy_data_input.PgDBConnect(*cfg)
-	if err != nil {
-		//logger.Error("Error connecting to database: %v", "error", err)
-		display_error("Error connecting to database", err)
-		return
-	}
-	defer db.Close()
-
-	proxy_list := proxy_data_input.GetProxies(db)
-
-	// // Using a for loop with an index
-	// for i := 0; i < len(proxy_list); i++ {
-	// 	fmt.Println(proxy_list[i])
-	// }
-
-	proxy_data_output.Check_stored_proxies(db, *cfg)
-	proxy_data_output.Check_proxies(db, *cfg, proxy_list)	
-
-	//logger.Debug(cfg.CheckURLEndPoint)
 }
